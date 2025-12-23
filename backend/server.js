871ed128c57c7c068c919e329fd457d2c6edfa11
Aish026 <66766409+Aish026@users.noreply.github.com>
2025-12-23 11:07:45 +0530

    Update CORS origin to allow all origins

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-  origin: 'https://file-sharing-app-vercel-one.vercel.app',
+  origin: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'auth']
 }));
